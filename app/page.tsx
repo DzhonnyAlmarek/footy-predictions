b963e67e8690d0bbbf250098e5489930d59e57bc
dm <dm@example.com>
2026-02-07 18:31:38 +0300

    UI: centered auth header, remove SiteHeader from login

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
 import LoginWidget from "./login-widget";
 
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
 export default async function HomePage() {
   return (
-    <main className="authCenter">
-      <div className="authWrap">
-        <h1 className="authTitle">Клуб им. А.Н. Мурашева</h1>
-        <div className="authSub">Прогнозы • таблицы • рейтинги</div>
+    <main className="authPage">
+      <div className="card authCard">
+        <div className="authBrand">
+          <div className="authTitle">Клуб им. А.Н. Мурашева</div>
+          <div className="authSubtitle">Прогнозы • таблицы • рейтинги</div>
+        </div>
 
-        <div className="authCard">
+        <div className="authHint">Выберите логин и введите пароль для входа</div>
+
+        <div className="cardBody">
           <LoginWidget />
         </div>
       </div>
